f9c0c03674a4cc7a973ab317931e95b45aaca138
Erick Afonso <36458464+erickafonso@users.noreply.github.com>
2023-09-06 09:59:12 -0300

    Update Criacao_Taabelas_curso-aluno-contato-disciplina.sql

--- Criacao_Taabelas_curso-aluno-contato-disciplina.sql
+++ Criacao_Taabelas_curso-aluno-contato-disciplina.sql
@@ -4,6 +4,7 @@ use faculdade;
 #CRIAÇÃO DE DO BANCO E TABELAS
 create database faculdade;
 use faculdade;
+
 create table curso(
 idCurso int primary key auto_increment not null,
 nome varchar(45) not null,
@@ -14,13 +15,31 @@ modalidade enum('ead','presencial') not null
 
 );
 
-create table curso(
-idCurso int primary key auto_increment not null,
+#reinserindo codigo que foi apagado
+	
+create table aluno(
+idAluno int primary key auto_increment not null,
+nome varchar(45) not null,
+cpf varchar(11) not null unique,
+data_nasc date not null,
+data_entrada date not null,
+data_saida date null
+);
+
+create table contato(
+idTelefone int primary key auto_increment not null,
+ddd varchar(2) not null,
+numero varchar(9) not null,
+email varchar(50)  null
+);
+
+create table disciplina(
+idDisciplina int primary key auto_increment not null,
 nome varchar(45) not null,
 duracao enum('3 meses','2 anos', '4 anos') not null,
-valor real not null,
 descricao varchar(255)  not null,
 modalidade enum('ead','presencial') not null
+);
 
 );
 
